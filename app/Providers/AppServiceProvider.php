b519b0f11c289475e030c08a607eed297651f93c
vagrant <vagrant@homestead>
2020-12-24 05:50:37 +0000

    引入支付库

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use Monolog\Logger;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema; //add fixed sql
+use Yansongda\Pay\Pay;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -14,7 +16,34 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton('alipay', function () {
+            $config = config('pay.alipay');
+
+            // 判断当前项目运行环境是否为线上环境
+            if (app()->environment() !== 'production') {
+                $config['mode'] = 'dev';
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+
+            //调用 Yansongda\Pay 来创建一个支付宝对象
+            return Pay::alipay($config);
+        });
+
+        $this->app->singleton('wechat', function () {
+            $config = config('pay.wechat');
+
+            // 判断当前项目运行环境是否为线上环境
+            if (app()->environment() !== 'production') {
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+
+            //调用 Yansongda\Pay 来创建一个支付宝对象
+            return Pay::wechat($config);
+        });
     }
 
     /**
