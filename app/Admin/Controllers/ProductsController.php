0435922f016e6cf4ae88e2bd82e7402d2fba9024
vagrant <vagrant@homestead>
2020-12-30 04:41:17 +0000

    管理后台 - 商品控制器代码封装

--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -10,8 +10,12 @@ use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
 
-class ProductsController extends AdminController
+class ProductsController extends CommonProductsController
 {
+    public function getProductType()
+    {
+        return Product::TYPE_NORMAL;
+    }
     /**
      * Title for current resource.
      *
@@ -23,16 +27,16 @@ class ProductsController extends AdminController
      * Make a grid builder.
      *
      * @return Grid
+     *
+     *
      */
-    protected function grid()
-    {
-        $grid = new Grid(new Product());
 
-        // 使用 with 来预加载商品类目数据，减少 SQL 查询
-        $grid->model()->where('type',Product::TYPE_NORMAL)->with(['category']);
+    protected function customGrid(Grid $grid)
+    {
+        $grid->model()->with(['category']);
 
         $grid->id('ID')->sortable();
-        $grid->title('商品名词');
+        $grid->title('商品名称');
         $grid->on_sale('已上架')->display(function ($value) {
             return $value ? '是' : '否';
         });
@@ -40,88 +44,8 @@ class ProductsController extends AdminController
         $grid->rating('评分');
         $grid->sold_count('销量');
         $grid->review_count('评论数');
-
-
-        $grid->actions(function ($actions) {
-            $actions->disableView();
-            $actions->disableDelete();
-        });
-        $grid->tools(function ($tools) {
-            // 禁用批量删除按钮
-            $tools->batch(function ($batch) {
-                $batch->disableDelete();
-            });
-        });
-
-        return $grid;
     }
-
-    /**
-     * Make a show builder.
-     *
-     * @param mixed $id
-     * @return Show
-     */
-    /**
-     * Make a form builder.
-     *
-     * @return Form
-     */
-    protected function form()
+    protected function customForm(Form $form)
     {
-        $form = new Form(new Product);
-        //水印路径
-        $watermark = public_path('watermark.png');
-        // 在表单中添加一个名为 type，值为 Product::TYPE_NORMAL 的隐藏字段
-        $form->hidden('type')->value(Product::TYPE_NORMAL);
-        // 创建一个输入框，第一个参数 title 是模型的字段名，第二个参数是该字段描述
-        $form->text('title', '商品名称')->rules('required');
-
-        // 创建一个选择图片的框
-        $form->image('image', '封面图片')->insert($watermark, 'content')->rules('required|image');
-
-        // 创建一个富文本编辑器
-        $form->quill('discription', '商品描述')->rules('required');
-
-        // 创建一组单选框
-        $form->radio('on_sale', '上架')->options(['1' => '是', '0' => '否'])->default('0');
-
-        //下拉库搜索功能
-        $form->select('category_id', '商品类目')->options(function ($id) {
-            $category = Category::find($id);
-            if ($category) {
-                return [$category->id => $category->item_name];
-            }
-        })->ajax('/admin/api/categories?is_directory=0');
-
-        //下拉框显示所有分类，无搜索
-        // $form->select('category_id', '商品类目')->options(function ($id) {
-        //     $categories = Category::query()->get();
-
-        //     $cates = [];
-        //     foreach ($categories as $category) {
-        //         $cates[$category->id] = $category->name;
-        //     }
-
-        //     return $cates;
-        // });
-
-        // 直接添加一对多的关联模型
-        $form->hasMany('skus', 'SKU 列表', function (Form\NestedForm $form) {
-            $form->text('title', 'SKU 名称')->rules('required');
-            $form->text('description', 'SKU 描述')->rules('required');
-            $form->text('price', '单价')->rules('required|numeric|min:0.01');
-            $form->text('stock', '剩余库存')->rules('required|integer|min:0');
-            $form->select('sku_category', 'sku 类别')->options(
-                ['1' => '颜色', '2' => '尺码']
-            );
-        });
-
-        // 定义事件回调，当模型即将保存时会触发这个回调
-        $form->saving(function (Form $form) {
-            $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price') ?: 0;
-        });
-
-        return $form;
     }
 }
